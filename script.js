2eca62d275bd3c62e9c5a13a68fb1999eae2ac1f
Pooja Nandguruwar <116483552+pooja-1205@users.noreply.github.com>
2024-10-26 21:39:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+      //  sticky navbar on scroll script  //
+      if (this.scrollY > 20) {
+        $(".navbar").addClass("sticky");
+      } else {
+        $(".navbar").removeClass("sticky");
+      }
+  
+      //  scroll-up button show/hide script  //
+      if (this.scrollY > 500) {
+        $(".scroll-up-btn").addClass("show");
+      } else {
+        $(".scroll-up-btn").removeClass("show");
+      }
+    });
+  
+    //  slide-up script  //
+  
+    $(".scroll-up-btn").click(function () {
+      $("html").animate({ scrollTop: 0 });
+      //  removing smooth scroll on slide-up button click  //
+      $("html").css("scrollBehavior", "auto");
+    });
+  
+    $(".navbar .menu li a").click(function () {
+      //  Smooth scroll on Menu Items click  //
+  
+      $("html").css("scrollBehavior", "smooth");
+    });
+  
+    //  Toggle Navbar  //
+  
+    $(".menu-btn").click(function () {
+      $(".navbar .menu").toggleClass("active");
+      $(".menu-btn i").toggleClass("active");
+    });
+  
+    //  Typing Text Animation  //
+  
+    var typed = new Typed(".typing", {
+      strings: [
+        "Graphic Designer",
+        "Machine Learning Enthusiast",
+        "Frontend Developer",
+        "Software Developer",
+        "Python Developer",
+        "Music Passionate"
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+  
+    var typed = new Typed(".typing-2", {
+      strings: [
+        "Graphic Designer",
+        "Machine Learning Enthusiast",
+        "Frontend Developer",
+        "Software Developer",
+        "Python Developer",
+        "Music Passionate"
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+  
+    //  Owl Carousel  //
+  
+    $(".carousel").owlCarousel({
+      margin: 20,
+      loop: true,
+      autoplay: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        600: {
+          items: 2,
+          nav: false
+        },
+        1000: {
+          items: 3,
+          nav: false
+        }
+      }
+    });
+  });
+
+  // my //
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+
+ 
+  
+
+  
